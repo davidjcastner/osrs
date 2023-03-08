c2b0cbe6e32c31de9eefd07dac5f4d4e64f758df
David Castner <davidjcastner@gmail.com>
2023-03-07 21:56:04 -0500

    Update settings.py

--- src/airtable/settings.py
+++ src/airtable/settings.py
@@ -6,6 +6,4 @@ LOAD_TIME = 0.4
 # other settings
 OSRS_TASK_URL = 'https://oldschool.runescape.wiki/w/Shattered_Relics_League/Tasks'
 OSRS_USERNAME = 'ValkyrieTail'
-AIRTABLE_API_KEY = 'keyC6JFjKNezmk7op'
-AIRTABLE_DATABASE_KEY = 'appZXCkfxkjKRR8eM'
 AIRTABLE_TABLE_NAME = 'League%20Tasks'
